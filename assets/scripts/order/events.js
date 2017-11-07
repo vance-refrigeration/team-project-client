400b7b53e47c9c88a80d3bac3dd6b37f6b5f2040
MJS6745 <Mark.Smets@libertymutual.com>
2017-11-07 16:37:29 -0500

    Added stripe functionality on order placement
    Stripe window now appears when customer clicks checkout on page. The
    order is placed and when a response is returned, the checkout window
    disappears

--- assets/scripts/order/events.js
+++ assets/scripts/order/events.js
@@ -2,6 +2,7 @@
 
 const api = require('./api')
 const ui = require('./ui')
+const store = require('../store')
 
 const onGetAllOrders = (event) => {
   // console.log('Get products event invoked')
@@ -21,11 +22,49 @@ const onCreateOrder = (event) => {
     .then(ui.createOrderSuccess)
     .catch(ui.createOrderFailure)
 }
+// Start of stripe code
+let tokenTriggered = false
 
+const stripeHandler = StripeCheckout.configure({
+  key: 'pk_test_n5itDkuM2xHubxhygUjisoph',
+  token: function (token) {
+    $('#stripeToken').val(token.id)
+    $('#stripeEmail').val(token.email)
+    tokenTriggered = true
+  },
+  closed: function () {
+    if (!tokenTriggered) {
+      console.log('Close button behavior here')
+    } else {
+      $('#cartModal').modal('hide')
+      console.log('Success!!')
+    }
+  }
+})
+
+const stripeSubmit = function () {
+  let cartTotal = 0
+  for (let i = 0; i < store.user.cart.length; i++) {
+    cartTotal += parseInt(store.user.cart[i].price)
+  }
+  cartTotal *= 100
+  stripeHandler.open({
+    name: 'Vance Refrigeration',
+    amount: cartTotal
+  })
+}
+
+const makePayment = function (event) {
+  event.preventDefault()
+  stripeSubmit()
+}
+
+// End of stripe code
 const addHandlers = () => {
   // $('#getOrders').on('click', onGetAllOrders)
   $('#createOrder').on('click', onCreateOrder)
   $('#getOrdersForm').on('submit', onGetAllOrders)
+  $('#checkout-button').on('click', makePayment)
   // need to add jquery for create order
 }
 
